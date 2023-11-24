e71836780b3cd89c9582d0f0478a74dfd492f6e5
Chyri <chyricpmb@gmail.com>
2023-11-24 11:45:33 -0300

    Add Biblioteca via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from livro import Livro
+from usuario import Usuario
+from emprestimo import Emprestimo
+from biblioteca import Biblioteca
+
+def menu_principal(biblioteca):
+    # ... (o resto do código permanece o mesmo)
+
+ if __name__ == "__main__":
+    biblioteca = Biblioteca()
+    menu_principal(biblioteca)
