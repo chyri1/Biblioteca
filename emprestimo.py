f8e8dce3869bf088f9948e20f9ed92b6fa1a8005
Chyri <chyricpmb@gmail.com>
2023-11-24 22:25:29 -0300

    Update emprestimo.py

---
+++ emprestimo.py
@@ -0,0 +1,6 @@
+class Emprestimo:
+    def __init__(self, usuario, livro, data_emprestimo, data_devolucao):
+        self.usuario = usuario
+        self.livro = livro
+        self.data_emprestimo = data_emprestimo
+        self.data_devolucao = data_devolucao
